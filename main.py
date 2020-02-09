7d68b83cecc6f23bbcc59e1a10da693932678079
Imran Macmillan <macmillan.i.g@gmail.com>
2020-02-09 11:31:59 -0800

    trying to restructure app for benefit of wtforms

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ import json
 import datetime
 
 from flask import Flask, render_template, request, jsonify
+from flask_wtf import FlaskForm
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+#class ContactMessageForm(FlaskForm):
+ #   first_name = wtforms.StringField(label="fname", validators=[wtforms.validators.DataRequired])
+ #   last_name = wtforms.StringField(label="lname", validators=[wtforms.validators.DataRequired])
+ #   message_body = wtforms.StringField(label="messagebody", validators=[wtforms.validators.DataRequired])
+
+class ContactMessageForm(FlaskForm):
+    firstname = StringField('fname', validators=[DataRequired()])
+    #lastname = StringField('lname')
+    messagebody = StringField('messagebody')
+    submit = SubmitField('Sign In')
 
 class JSONEncoder(json.JSONEncoder):
     ''' extend json-encoder class'''
@@ -45,9 +59,15 @@ def actiontest():
     if request.method == 'POST':
         comms_collection = mongo.db.comms
         print("----------POST begins--------------")
-        query2 = request.form['messagebody']
-        print(query2)
-        comms_collection.insert_one({'firstname' : 'Test', 'lastname': 'Guy', 'email': '1', 'address': '2', 'city': '','country':'','reason':'', 'message':query2})
+        messagebody = request.form['messagebody']
+        firstname = request.form['fname']
+        #lname = request.form['lname']
+        #email = request.form['email']
+        #address = request.form['address']
+        #city = request.form['city']
+        print(messagebody)
+        #comms_collection.insert_one({'firstname' :fname, 'lastname':lname, 'email':email, 'address':address, 'city':city,'country':country,'reason':'', 'message':messagebody})
+        comms_collection.insert_one({'firstname' :'', 'lastname':'', 'email':'', 'address':'', 'city':'','country':'','reason':'', 'message':messagebody})
         print("----------------POST COMPLETED--------------")
         return render_template("contactme.html"), 200
     else:
